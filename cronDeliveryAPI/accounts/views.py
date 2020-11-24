a2a77e94c36ce06ede74d8331f11b86c2ffe3d41
murkhan13 <merch.fade13@gmail.com>
2020-11-24 14:21:08 +0300

    hosts crondostav.ru included

--- cronDeliveryAPI/accounts/views.py
+++ cronDeliveryAPI/accounts/views.py
@@ -27,8 +27,8 @@ def send_sms(phone, key):
     print(phone)
     login = 'CronApp'
     password = 'croncron'
-    message = "DeliveryAPPCode - код подтверждения:"
-    message += str(key) # DeliveryAPPCode:key
+    message = "CronDostavka - код подтверждения:"
+    message += str(key) # CronDostavka:key
     link = "http://smsc.ru/sys/send.php?login=%s&psw=%s&phones=%s&mes=%s" % (login, password, phone, message)
     print(link)
 
